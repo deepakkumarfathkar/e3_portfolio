fb43ef8a676f16867ebded7a62b4242b070ee52a
Deepak Kumar Fathkar <deepak.fathkar@gmail.com>
2023-01-09 14:19:13 +0530

    done

--- pages/experience/index.js
+++ pages/experience/index.js
@@ -1,26 +1,27 @@
-import React from 'react'
+import React from "react";
 import { username } from "../index.js";
-import { Center, Container, Heading } from '@chakra-ui/react';
+import { Center, Container, Heading } from "@chakra-ui/react";
 
-const Experience = ({data}) => {
+const Experience = ({ data }) => {
   return (
     <Container maxW={"100%"} bgColor="gray.300" pt={5}>
-        <Center>
-            <Heading mt={"20%"} mb={"30%"}>{data.bio}</Heading>
-        </Center>
+      <Center>
+        <Heading mt={"20%"} mb={"30%"}>
+          {data.bio}
+        </Heading>
+      </Center>
     </Container>
-  )
-}
-
+  );
+};
 
 export async function getStaticProps() {
-    let resp = await fetch(`https://api.github.com/users/${username}`);
-    let data_received = await resp.json();
-    return {
-      props: {
-        data: data_received,
-      },
-    };
-  }
+  let resp = await fetch(`https://api.github.com/users/${username}`);
+  let data_received = await resp.json();
+  return {
+    props: {
+      data: data_received,
+    },
+  };
+}
 
-export default Experience
+export default Experience;
